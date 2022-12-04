1ef5597e32a6c80fdf19ec2c88c6a6cc560398d2
soham <sohamb2019@gmail.com>
2022-12-04 23:05:28 +0530

    Tested game run bug found..

--- snake.h
+++ snake.h
@@ -149,7 +149,7 @@ void logic(int *flag,int *x,int *y,int *gameover,int *height,int *width,int *fru
 
 void draw(int *x,int *y,int *i,int *j,int *width,int *height,int *fruitx,int *fruity,int *score)
 {
-    system(R"(printf \'\\033\')");
+    system(R"(printf '\033')");
     for (*i = 0; *i < *height; *i+=1) {
         for (*j = 0; *j < *width; *j+=1) {
             if (*i == 0 || *i == *width - 1
@@ -170,11 +170,6 @@ void draw(int *x,int *y,int *i,int *j,int *width,int *height,int *fruitx,int *fr
         printf("\n");
         }
 
-    // Print the score after the
-    // game ends
-    printf("score = %d", *score);
-    printf("\n");
-    printf("press X to quit the game\n");
 }
 
 #endif //GAME_SNAKE_H
